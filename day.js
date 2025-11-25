31be586177313d65e702ffb3a190fe90739c6e01
Christian7898 <christiandifede777@gmail.com>
2025-11-25 22:06:20 +0100

    Commit con la possibilità di rimuovere i record

--- day.js
+++ day.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getFirestore, collection, getDocs, deleteDoc, doc} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDiDYvcCFMtkCTZGVRWuAkr8OSgXn4dCtI",
@@ -13,13 +13,21 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-function formatItalian(date) {
-  return date.toLocaleDateString("it-IT", {
+function formatItalian(date, id) {
+  const formatted = date.toLocaleDateString("it-IT", {
     day: "numeric",
     month: "long",
     year: "numeric"
   });
+
+  return `${formatted} (ID: ${id})`;
 }
+
+async function deleteRecord(id){
+  await deleteDoc(doc(db, "Lentine", id));
+  console.log("ELIMINATO ", id);
+}
+
 
 async function loadDays() {
   const list = document.querySelector(".days-list");
@@ -36,23 +44,43 @@ async function loadDays() {
     const dataCreazione = data.dataCreazioneRecord.toDate();
 
     const html = `
-      <div class="day-card">
-        <div class="day-main">
-          <span class="day-date">${formatItalian(dataUtil)}</span>
-          <span class="day-sub">Creato il ${formatItalian(dataCreazione)}</span>
-        </div>
-        <div class="day-meta">
-          <strong>${data.oreUtilizzo} ore</strong>
-          <span class="chip">${data.tipoLenti === "monthly" ? "Mensili" : data.tipoLenti}</span>
-        </div>
+    <div class="day-card" data-id="${docu.id}">
+      <div class="day-main">
+        <span class="day-date">${formatItalian(dataUtil, docu.id)}</span>
+        <span class="day-sub">Creato il ${formatItalian(dataCreazione)}</span>
       </div>
-    `;
+      <div class="day-meta">
+        <strong>${data.oreUtilizzo} ore</strong>
+        <span class="chip">${data.tipoLenti === "monthly" ? "Mensili" : data.tipoLenti}</span>
+      </div>
+    </div>
+  `;
 
     list.insertAdjacentHTML("beforeend", html);
+
+    // prende l’ultimo elemento inserito
+    const card = list.lastElementChild;
+    const btnDel = document.getElementById('btnDelate');
+    card.addEventListener("click", () => {
+      
+      btnDel.style.display = 'initial';
+      btnDel.textContent = 'Elimina giorno ' +docu.id;
+      console.log("Hai cliccato:", docu.id);   
+      btnDel.addEventListener("click", () =>{
+        deleteRecord(docu.id);
+
+        setTimeout(function(){
+          location.reload();
+        }, 2000);
+
+      });   
+    });
+
     count++;
   });
 
   document.querySelector(".days-summary strong").textContent = count;
+
 }
 
 document.addEventListener("DOMContentLoaded", loadDays);
